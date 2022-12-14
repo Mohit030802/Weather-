061bed804192b99e5e96dac26e9e32de9462dde7
Mohit030802 <mohitom2002@gmail.com>
2022-12-14 21:03:53 +0530

    different city add

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const options = {
 const getWeather=(city)=>{
 cityName.innerHTML=city
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
 	.then(response => response.json())
 	.then(response =>{ 
         console.log(response)
        
-            cloud_pct.innerHTML=response.cloud_pct
+            
             temp.innerHTML=response.temp
             feels_like.innerHTML=response.feels_like
             humidity.innerHTML=response.humidity
@@ -31,3 +31,20 @@ submit.addEventListener("click",(e)=>{
     getWeather(city.value)
 })
 getWeather("Delhi")
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Shanghai', options)
+	.then(response => response.json())
+	.then(response =>{ 
+        console.log(response)
+       
+            
+            
+            humidity2.innerHTML=response.humidity2
+            min_temp2.innerHTML=response.min_temp2
+            max_temp2.innerHTML=response.max_temp2
+            
+           
+          
+    })
+	.catch(err => console.error(err)); 
+
+
